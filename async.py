cadc25c466eb772539be5cf5935f1bd2fbe18481
Femarko <fenstermark7@gmail.com>
2024-02-05 18:58:01 +0100

    check of persons quatity in the process

--- async.py
+++ async.py
@@ -17,9 +17,10 @@ async def quantity(base_url: str, session: ClientSession) -> int:
     return json.get("count")
 
 
-async def get_persons(base_url: str, session: ClientSession) -> dict:
-    persons_quantity = await quantity(base_url, session)
-    coro_list = [session.get(f"{base_url}/{person_id}") for person_id in range(1, persons_quantity + 1)]
+async def get_persons(base_url: str, session: ClientSession, first_id=1, last_id=None) -> dict:
+    if not last_id:
+        last_id = await quantity(base_url, session)
+    coro_list = [session.get(f"{base_url}/{person_id}") for person_id in range(first_id, last_id + 1)]
     response_list = await asyncio.gather(*coro_list)
     persons_dict = {
         int(str(item.url).split("//")[2]): await item.json() for item in response_list if item.status == 200
@@ -27,14 +28,19 @@ async def get_persons(base_url: str, session: ClientSession) -> dict:
     return persons_dict
 
 
-async def check_quantity(quantity: str, session: ClientSession):
-    if get_persons():
-        pass
-
-
 async def main():
     async with ClientSession() as session:
-        return await get_persons(base_url, session)
+        persons_quantity = await quantity(base_url, session)
+        persons_dict = await get_persons(base_url, session, persons_quantity)
+        if len(persons_dict.keys()) < persons_quantity:
+            first_id = max(persons_dict.keys()) + 1
+            last_id = first_id + 1
+            while len(persons_dict.keys()) < persons_quantity:
+                new_person = await get_persons(base_url, session, first_id=first_id, last_id=last_id)
+                persons_dict = persons_dict | new_person
+            return persons_dict
+        return persons_dict
+
 
 if __name__ == '__main__':
     pprint(asyncio.run(main()))
