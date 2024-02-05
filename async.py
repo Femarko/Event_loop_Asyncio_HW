d3f3121a88d71fcd2f7d91b8dc5285c95fb8fa66
Femarko <fenstermark7@gmail.com>
2024-02-05 20:14:43 +0100

    add_person() added

--- async.py
+++ async.py
@@ -11,15 +11,15 @@ if platform.system()=='Windows':
 CHUNK_SIZE = 10
 
 
-async def quantity(base_url: str, session: ClientSession) -> int:
+async def persons_quantity(base_url: str, session: ClientSession) -> int:
     response = await session.get(f"{base_url}")
     json = await response.json()
     return json.get("count")
 
 
-async def get_persons(base_url: str, session: ClientSession, first_id=1, last_id=None) -> dict:
+async def get_persons(base_url: str, session: ClientSession, first_id: int=1, last_id: int=None) -> dict:
     if not last_id:
-        last_id = await quantity(base_url, session)
+        last_id = await persons_quantity(base_url, session)
     coro_list = [session.get(f"{base_url}/{person_id}") for person_id in range(first_id, last_id + 1)]
     response_list = await asyncio.gather(*coro_list)
     persons_dict = {
@@ -28,19 +28,29 @@ async def get_persons(base_url: str, session: ClientSession, first_id=1, last_id
     return persons_dict
 
 
-async def main():
-    async with ClientSession() as session:
-        persons_quantity = await quantity(base_url, session)
-        persons_dict = await get_persons(base_url, session, persons_quantity)
-        if len(persons_dict.keys()) < persons_quantity:
-            first_id = max(persons_dict.keys()) + 1
-            last_id = first_id + 1
-            while len(persons_dict.keys()) < persons_quantity:
-                new_person = await get_persons(base_url, session, first_id=first_id, last_id=last_id)
-                persons_dict = persons_dict | new_person
-            return persons_dict
+async def add_persons(
+        base_url: str,
+        persons_quantity: int,
+        persons_dict: dict,
+        session: ClientSession,
+        first_id: int=1,
+        last_id: int=None
+) -> dict:
+        first_id = max(persons_dict.keys()) + 1
+        last_id = first_id + 1
+        while len(persons_dict.keys()) < persons_quantity:
+            new_person = await get_persons(base_url, session, first_id=first_id, last_id=last_id)
+            persons_dict = persons_dict | new_person
         return persons_dict
 
+
+async def main():
+    async with ClientSession() as session:
+        persons_quantiy = await persons_quantity(base_url, session)
+        persons_dict = await get_persons(base_url, session)
+        if len(persons_dict.keys()) < persons_quantiy:
+            persons_dict = await add_persons(base_url, persons_quantiy, persons_dict, session)
+        return persons_dict
 
 if __name__ == '__main__':
     pprint(asyncio.run(main()))
