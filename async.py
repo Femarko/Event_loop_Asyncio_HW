f2b13c079b465c6f8e34e411df4d1a661231d48b
Femarko <fenstermark7@gmail.com>
2024-02-06 01:31:46 +0100

    getting attributes in the process

--- async.py
+++ async.py
@@ -1,4 +1,6 @@
 import asyncio, aiohttp, requests
+from datetime import datetime
+
 from aiohttp import ClientSession
 from more_itertools import chunked
 from input_data import base_url, attributes_to_get
@@ -17,9 +19,9 @@ async def persons_quantity(base_url: str, session: ClientSession) -> int:
     return json.get("count")
 
 
-async def get_persons(base_url: str, session: ClientSession, first_id: int=1, last_id: int=None) -> list:
+async def get_persons(base_url: str, persons_quantity, session: ClientSession, first_id: int=1, last_id: int=None) -> list:
     if not last_id:
-        last_id = await persons_quantity(base_url, session)
+        last_id = persons_quantity
     coro_list = [session.get(f"{base_url}/{person_id}") for person_id in range(first_id, last_id + 1)]
     response_list = await asyncio.gather(*coro_list)
     # persons_dict = {
@@ -38,34 +40,35 @@ async def add_persons(
         session: ClientSession,
         first_id: int=1,
         last_id: int=None
-) -> dict:
+) -> list:
         first_id = persons_list[-1]["id"] + 1
         last_id = first_id + 1
         while len(persons_list) < persons_quantity:
-            new_person = await get_persons(base_url, session, first_id=first_id, last_id=last_id)
-            persons_list.append(new_person)
+            new_person = await get_persons(base_url, persons_quantity, session, first_id=first_id, last_id=last_id)
+            persons_list = [*persons_list, *new_person]
         return persons_list
 
 
-async def get_attributes(base_url: str, persons_list: list, session: ClientSession):
-    for item in persons_list:
-        for key, value in attributes_to_get:
-            for attr_url in item[key]:
-                item[key] = ",".join([session.get(f'{attr_url}').json()[value]
+async def get_attributes(base_url: str, persons_list: list, session: ClientSession=None):
+    atrr_dict = dict.fromkeys(attributes_to_get.keys())
+    return {str(type(item)): item for item in persons_list}
+
 
 
 async def main():
     async with ClientSession() as session:
         persons_quantiy = await persons_quantity(base_url, session)
-        persons_list = await get_persons(base_url, session)
+        persons_list = await get_persons(base_url, persons_quantiy, session)
         if len(persons_list) < persons_quantiy:
             persons_list = await add_persons(base_url, persons_quantiy, persons_list, session)
-        return persons_list
-
+    for item in persons_list:
+        print(f"{type(item) = }")
 # async def main():
 #     async with ClientSession() as session:
 #         return await get_persons(base_url, session)
 
 
 if __name__ == '__main__':
-    pprint(asyncio.run(main()))
+    start_time = datetime.now()
+    asyncio.run(main())
+    print(datetime.now() - start_time)
