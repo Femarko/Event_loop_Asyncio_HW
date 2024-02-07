cbb15e058a39ada9be3565ea17d4407655103a5c
Femarko <fenstermark7@gmail.com>
2024-02-07 18:17:45 +0100

    Chunked implemented, add_persons() does not work yet

--- swapi_async.py
+++ swapi_async.py
@@ -19,10 +19,10 @@ async def get_field_value(base_url: str, field: str, session: ClientSession) -> 
     return json.get(field)
 
 
-async def get_persons(base_url: str, persons_quantity, session: ClientSession, first_id: int=1, last_id: int=None) -> list:
-    if not last_id:
-        last_id = persons_quantity
-    coro_list = [session.get(f"{base_url}/{person_id}") for person_id in range(first_id, last_id + 1)]
+async def get_persons(base_url: str, session: ClientSession, id_list: list) -> list:
+    # if not last_id:
+    #     last_id = persons_quantity
+    coro_list = [session.get(f"{base_url}/{person_id}") for person_id in id_list]
     response_list = await asyncio.gather(*coro_list)
     persons_list = []
     for item in response_list:
@@ -32,18 +32,12 @@ async def get_persons(base_url: str, persons_quantity, session: ClientSession, f
     return persons_list
 
 
-async def add_persons(
-        base_url: str,
-        persons_quantity: int,
-        persons_list: list,
-        session: ClientSession,
-        first_id: int=1,
-        last_id: int=None
-) -> list:
-        first_id = persons_list[-1]["id"] + 1
-        last_id = first_id + 1
+async def add_persons(base_url: str, persons_quantity: int, persons_list: list, session: ClientSession) -> list:
+        id_list = [persons_list[-1]["id"] + 1, persons_list[-1]["id"] + 2]
+        if len(id_list) > CHUNK_SIZE:
+            pass
         while len(persons_list) < persons_quantity:
-            new_person = await get_persons(base_url, persons_quantity, session, first_id=first_id, last_id=last_id)
+            new_person = await get_persons(base_url, session, id_list)
             persons_list = [*persons_list, *new_person]
         return persons_list
 
@@ -59,13 +53,17 @@ async def get_attributes(base_url: str, persons_list: list, session: ClientSessi
 
 async def main():
     async with ClientSession() as session:
-        persons_quantiy = await get_field_value(base_url, "count", session)
-        persons_list = await get_persons(base_url, persons_quantiy, session)
-        if len(persons_list) < persons_quantiy:
-            persons_list = await add_persons(base_url, persons_quantiy, persons_list, session)
-        attr_list = await get_attributes(base_url, persons_list, session)
-        pprint(attr_list)
-
+        persons_quantity = await get_field_value(base_url, "count", session)
+        persons_list = []
+        for id_list in chunked(range(1, persons_quantity +1), CHUNK_SIZE):
+            persons_list_chunk = await get_persons(base_url, session, id_list)
+            persons_list_chunk = await get_attributes(base_url, persons_list_chunk, session)
+            persons_list = [*persons_list, *persons_list_chunk]
+        # if len(persons_list) < persons_quantity:
+        #     id_list = [persons_list[-1]["id"] + 1, persons_list[-1]["id"] + 2]
+        #     if len(id_list) > CHUNK_SIZE:
+        #     persons_list = await add_persons(base_url, persons_quantity, persons_list, session)
+        pprint(persons_list)
 # async def main():
 #     async with ClientSession() as session:
 #         return await get_field_value(base_url, "count", session)
@@ -75,3 +73,4 @@ if __name__ == '__main__':
     start_time = datetime.now()
     asyncio.run(main())
     print(datetime.now() - start_time)
+    # print(type(CHUNK_SIZE))
